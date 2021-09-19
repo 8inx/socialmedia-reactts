d0e2d65f8c6ef1be1a3ffe43f1de06d01cfecec8
coltx86 <acebo.dale@gmail.com>
2021-09-19 20:06:35 +0800

    Shorcuts

---
+++ src/components/Shortcuts/index.tsx
@@ -0,0 +1,11 @@
+import PaneTitle from "shared/PaneTitle";
+
+const Shortcuts = () => {
+  return (
+      <>
+        <PaneTitle>Shorcuts</PaneTitle>
+      </>
+  )
+}
+
+export default Shortcuts;

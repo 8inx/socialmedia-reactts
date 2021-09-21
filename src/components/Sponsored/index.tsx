6085569940fc7e59926182ef2cb49f741048cb57
coltx86 <acebo.dale@gmail.com>
2021-09-21 17:03:24 +0800

    update sponsored

--- src/components/Sponsored/index.tsx
+++ src/components/Sponsored/index.tsx
@@ -3,14 +3,20 @@ import ImageAd1 from "assets/image/ad1.jpg"
 import ImageAd2 from "assets/image/ad2.jpg"
 import { Link } from "react-router-dom";
 import PaneTitle from "shared/PaneTitle";
+import Pressable from "shared/Pressable";
 
-const FlexWrapper = styled.div`
+const StyledLink = styled(Link)`
   display: flex;
   align-items: center;
   padding: 8px;
   gap: 8px;
+  color: ${props => props.theme.color.text.primary};
+  border-radius: 8px;
+  transition: all 300ms;
+  &:hover {
+    background: ${props => props.theme.color.divider};
+  }
 `
-
 
 const StyledImage = styled.img`
   width: 100px;
@@ -18,29 +24,24 @@ const StyledImage = styled.img`
   border-radius: 4px;
   object-fit: cover;
 `
-
-const StyledLink = styled(Link)`
-  color: ${props => props.theme.color.text.primary};
-`
-
 const Sponsored = () => {
   return (
       <>
         <PaneTitle>Sponsored</PaneTitle>
-        <FlexWrapper>
+        <StyledLink to="/">
           <StyledImage src={ImageAd1}/>
-          <StyledLink to="/">
+          <span>
             <h4>Shopee</h4>
             <small>ads.shopee.com</small>
-          </StyledLink>
-        </FlexWrapper>
-        <FlexWrapper>
+          </span>
+        </StyledLink>
+        <StyledLink to="/">
           <StyledImage src={ImageAd2}/>
-          <StyledLink to="/">
+          <span>
             <h4>Huawei Mobile</h4>
             <small>ads.huawei.com</small>
-          </StyledLink>
-        </FlexWrapper>
+          </span>
+        </StyledLink>
       </>
   )
 }
